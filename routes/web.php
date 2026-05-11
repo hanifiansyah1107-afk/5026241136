8899d6c197c30a67ec83e5330b91963475bd51bb
Muhammad Hanifiansyah <Hanifiansyah1107@gmail.com>
2026-05-11 12:00:36 +0700

    pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -41,3 +44,15 @@ Route::get('pert4', function () {
 Route::get('main', function () {
     return view('main');
 });
+
+Route::get('/pegawai/nama', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+// Blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+// CRUD Tabel Pegawai
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
